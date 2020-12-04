b2d54db21d138444bfe1c584c0b91f84f1bfe1d4
Gabriel <knuckles4x@gmail.com>
2020-12-04 19:51:40 -0300

    add recharts

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # O problema
 
 - Crie uma aplicação web com um gráfico que mostre a taxa de câmbio do real nos últimos 30 dias em relação a outras moedas;
-- Caso amoeda selecionada seja o dólar, os valores mostrados precisam ser algo como 5.38, 5.41, 5.33 e assim por diante;
+- Caso a moeda selecionada seja o dólar, os valores mostrados precisam ser algo como 5.38, 5.41, 5.33 e assim por diante;
 - Consumir aa api exchangerate.host;
 - Deve ser possível escolher qualquer moeda suportada pela API;
 - Os valores devem ser mostrados em um gráfico;
-- Com a mudaçã de moeda o gráfico deverá ser atualizado sem a necessidade de recarregar a página;
+- Com a mudança de moeda o gráfico deverá ser atualizado sem a necessidade de recarregar a página;
 
-# Run
+# Tasks
+
+[x] Fazer o gráfico
+[ ] Fazer funcionar com uma fake response
+[ ] Integrar com a API
--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import {
+  LineChart,
+  XAxis,
+  Tooltip,
+  CartesianGrid,
+  Line,
+  YAxis,
+  Legend,
+} from "recharts";
+import api from "./services/api";
+
 function App() {
-  return <h1>Hello</h1>;
+  const [timeseries, setTimeseries] = useState({});
+
+  useEffect(() => {
+    async function getApiData() {
+      const response = await api.get(
+        "timeseries?start_date=2020-01-01&end_date=2020-01-04&base=BRL"
+      );
+      console.log(response.data);
+      setTimeseries(response.data.rates);
+    }
+    getApiData();
+  }, []);
+
+  const data = [
+    { name: "Page A", uv: 10, pv: 20 },
+    { name: "Page B", uv: 20, pv: 40 },
+    { name: "Page A", uv: 30, pv: 60 },
+    { name: "Page A", uv: 5, pv: 10 },
+  ];
+
+  return (
+    <LineChart width={500} height={300} data={data}>
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+      <Legend />
+      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+    </LineChart>
+  );
 }
 
 export default App;
