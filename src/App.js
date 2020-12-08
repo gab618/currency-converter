1abf883221e6393e55fa4fe9b2a83f065e23a46b
Gabriel <knuckles4x@gmail.com>
2020-12-08 00:00:58 -0300

    all currencies working

--- src/App.js
+++ src/App.js
@@ -12,11 +12,134 @@ import api from "./services/api";
 
 function App() {
   const [timeseries, setTimeseries] = useState([]);
+  const [base, setBase] = useState("USD");
+  const [compared, setCompared] = useState("BRL");
+
+  const options = [
+    "USD",
+    "JPY",
+    "BGN",
+    "CZK",
+    "DKK",
+    "GBP",
+    "HUF",
+    "PLN",
+    "RON",
+    "SEK",
+    "CHF",
+    "ISK",
+    "NOK",
+    "HRK",
+    "RUB",
+    "TRY",
+    "AUD",
+    "BRL",
+    "CAD",
+    "CNY",
+    "HKD",
+    "IDR",
+    "ILS",
+    "INR",
+    "KRW",
+    "MXN",
+    "MYR",
+    "NZD",
+    "PHP",
+    "SGD",
+    "THB",
+    "ZAR",
+    "EUR",
+    "AED",
+    "AFN",
+    "ALL",
+    "ARS",
+    "BAM",
+    "BBD",
+    "BDT",
+    "BHD",
+    "BIF",
+    "BMD",
+    "BND",
+    "BOB",
+    "BSD",
+    "BWP",
+    "BZD",
+    "CLP",
+    "COP",
+    "CRC",
+    "CUP",
+    "CVE",
+    "DJF",
+    "DOP",
+    "DZD",
+    "EGP",
+    "ETB",
+    "FJD",
+    "GHS",
+    "GNF",
+    "GTQ",
+    "HNL",
+    "HTG",
+    "IQD",
+    "JMD",
+    "JOD",
+    "KES",
+    "KHR",
+    "KWD",
+    "KYD",
+    "KZT",
+    "LAK",
+    "LBP",
+    "LKR",
+    "LSL",
+    "LYD",
+    "MAD",
+    "MDL",
+    "MGA",
+    "MKD",
+    "MMK",
+    "MOP",
+    "MUR",
+    "MVR",
+    "MWK",
+    "NAD",
+    "NGN",
+    "NIO",
+    "NPR",
+    "OMR",
+    "PAB",
+    "PEN",
+    "PGK",
+    "PKR",
+    "PYG",
+    "QAR",
+    "RSD",
+    "RWF",
+    "SAR",
+    "SCR",
+    "SDG",
+    "SOS",
+    "SVC",
+    "SZL",
+    "TND",
+    "TTD",
+    "TWD",
+    "TZS",
+    "UAH",
+    "UGX",
+    "UYU",
+    "UZS",
+    "VND",
+    "XAF",
+    "XOF",
+    "XPF",
+    "ZMW",
+  ];
 
   useEffect(() => {
     async function getApiData() {
       const response = await api.get(
-        "timeseries?start_date=2020-11-07&end_date=2020-12-07&base=USD"
+        `timeseries?start_date=2020-11-07&end_date=2020-12-07&base=${base}`
       );
 
       const formattedData = [];
@@ -29,29 +152,46 @@ function App() {
       setTimeseries(formattedData);
     }
     getApiData();
-  }, []);
+  }, [base]);
 
-  const data = [
-    { day: "2020-01-01", value: 0.249173 },
-    { day: "2020-01-02", value: 0.250006 },
-    { day: "2020-01-03", value: 0.248687 },
-    { day: "2020-01-04", value: 0.248687 },
-  ];
+  function handleSelectBase(e) {
+    setBase(e.target.value);
+  }
+  function handleSelectCompared(e) {
+    setCompared(e.target.value);
+  }
 
   return (
-    <LineChart width={800} height={500} data={timeseries}>
-      <XAxis dataKey="day" />
-      <YAxis />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="value.BRL"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
-      />
-    </LineChart>
+    <>
+      <LineChart width={800} height={600} data={timeseries}>
+        <XAxis dataKey="day" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Legend />
+        <Line
+          name={compared}
+          type="monotone"
+          dataKey={`value.${compared}`}
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+      <select name="base" id="base" onChange={handleSelectBase}>
+        {options.map((option) => (
+          <option key={`b-${option}`} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <select name="compared" id="compared" onChange={handleSelectCompared}>
+        {options.map((option) => (
+          <option key={`c-${option}`} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </>
   );
 }
 
